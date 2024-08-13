51afceda41a810da41e2821435fe505326acbb65
PariBai <paribai.sthpf20@ibacc.edu.pk>
2024-08-13 23:57:51 +0500

    Removed Unwanted Files

--- myproject/src/CustomComponent.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-function CustomComponent(){
- return <h1>MY component</h1>
-}
-export default CustomComponent;
--- n2.js
+++
@@ -1,2 +0,0 @@
-console.log("hello world")
-document.cookie = "username=John doe"
